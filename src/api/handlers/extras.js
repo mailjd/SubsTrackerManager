fd4ffe59136b7ca8146ccd464015d7ae69f5126a
mailjd <mailjd@xyprod.bond>
2026-09-23 16:01:28 +0800

    Fix Cloudflare Pages build v3.3.6

--- src/api/handlers/extras.js
+++ src/api/handlers/extras.js
@@ -24,7 +24,7 @@ import { getCategories, addCategory } from '../../data/categories.js';
 import { getMenuOptions, addMenuOption, removeMenuOption, resetMenuOptions, isValidMenuGroup } from '../../data/menu-options.js';
 import { getNextFireTime } from '../../services/notify/reminder-engine.js';
 
-export const VERSION = '3.3.7';
+export const VERSION = '3.3.6';
 
 /** 标准 JSON 响应 */
 function json(data, status = 200) {
